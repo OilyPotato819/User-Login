abd22000fbaddddad994c30d50bed30518fdab57
Oliver Schneider <o.schneider@EDU.epsb.ca>
2021-09-21 14:49:07 -0600

    on to js

---
+++ script.js
@@ -0,0 +1,16 @@
+// User Login
+
+// Variables for HTML Elements
+
+
+// Add Event Listener
+document.getElementById("login-btn").addEventListener("click", login);
+
+// Event Function
+function login() {
+    // Get Input
+ let username = document.getElementById("user-in").value;
+
+    // Check the name
+    
+}
