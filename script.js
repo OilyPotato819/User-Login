96b51dc9a73bbe237f97255538c6523dfb9d7a0e
Oliver Schneider <o.schneider@EDU.epsb.ca>
2021-09-22 14:02:35 -0600

    done

--- script.js
+++ script.js
@@ -9,8 +9,21 @@ document.getElementById("login-btn").addEventListener("click", login);
 // Event Function
 function login() {
     // Get Input
- let username = document.getElementById("user-in").value;
+    let username = document.getElementById("user-in").value;
+    let password = document.getElementById("pass-in").value;
 
-    // Check the name
-    
+    // Check the info
+    if (username === "admin" && password === "1234") {
+        alert("Login Successful");
+    } else if (username === "" && password === "") {
+        alert("Please enter your username and password")
+    } else if (username === "") {
+        alert("Please enter your username")
+    } else if (password === "") {
+        alert("Please enter your password")
+    } else if (username !== "admin") {
+        alert("Invalid Username");
+    } else if (password !== "1234") {
+        alert("Invalid Password");
+    }
 }
